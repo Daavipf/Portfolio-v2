45b40a5dbc2d057025bd8b2faeceb3ce23464676
Daavipf <davi.pferreira2705@gmail.com>
2025-07-31 23:39:13 -0300

    Added target blank to links

--- src/components/IconsList.tsx
+++ src/components/IconsList.tsx
@@ -7,22 +7,22 @@ export default function IconsList() {
   return (
     <ul className="flex items-center gap-2">
       <li>
-        <a href="https://github.com/Daavipf">
+        <a target="blank" href="https://github.com/Daavipf">
           <img src={GitHub} alt="" width={48} />
         </a>
       </li>
       <li>
-        <a href="https://www.instagram.com/davi.pferreira2705?igsh=b3Q4aXMwandvcHVi">
+        <a target="blank" href="https://www.instagram.com/davi.pferreira2705?igsh=b3Q4aXMwandvcHVi">
           <img src={Instagram} alt="" width={48} />
         </a>
       </li>
       <li>
-        <a href="https://www.linkedin.com/in/davi-pereira-3862a9282/">
+        <a target="blank" href="https://www.linkedin.com/in/davi-pereira-3862a9282/">
           <img src={LinkedIn} alt="" width={48} />
         </a>
       </li>
       <li>
-        <a href="https://wa.link/aags5k">
+        <a target="blank" href="https://wa.link/aags5k">
           <img src={Whatsapp} alt="" width={48} />
         </a>
       </li>
